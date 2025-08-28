55d9b7758852919b683031252c031d7dd9609ea7
Bruno Pozenato <bruno@busify.com>
2025-08-28 17:25:59 -0300

    Updates

---
+++ precache-manifest.6e02cb344d294a56092b29eb1d839900.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4053e644fb241799d1f3c240bc0d91",
+    "url": "/ACME-Chat/index.html"
+  },
+  {
+    "revision": "2b32dc5a3be1e529bcad",
+    "url": "/ACME-Chat/static/css/2.e515a48f.chunk.css"
+  },
+  {
+    "revision": "b8e556bddabc1998ebd4",
+    "url": "/ACME-Chat/static/css/main.2510b1ec.chunk.css"
+  },
+  {
+    "revision": "2b32dc5a3be1e529bcad",
+    "url": "/ACME-Chat/static/js/2.d871712d.chunk.js"
+  },
+  {
+    "revision": "b8e556bddabc1998ebd4",
+    "url": "/ACME-Chat/static/js/main.d922aa51.chunk.js"
+  },
+  {
+    "revision": "3bfbd34680a81ab3666c",
+    "url": "/ACME-Chat/static/js/runtime~main.287149db.js"
+  },
+  {
+    "revision": "cf2842ec05dc062e6a7c67ff09e83fd8",
+    "url": "/ACME-Chat/static/media/Acme-Regular.cf2842ec.ttf"
+  },
+  {
+    "revision": "82be7c713e89b94d2a6f6a8c16bacf03",
+    "url": "/ACME-Chat/static/media/Acme-corp.82be7c71.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4950f7541cbd2d1930db28e3c57bba23.js"
+  "/ACME-Chat/precache-manifest.6e02cb344d294a56092b29eb1d839900.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ACME-Chat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
