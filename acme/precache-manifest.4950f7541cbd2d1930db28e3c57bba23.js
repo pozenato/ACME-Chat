88a0a9c61b84138512d9dff4976292f7dac1f63e
Bruno Pozenato <bruno@busify.com>
2025-08-28 17:21:19 -0300

    Deploy to GitHub Pages

---
+++ acme/precache-manifest.4950f7541cbd2d1930db28e3c57bba23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6662f36036eda57b627cfe43bc30767a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a1a693d4f01e1e214d46",
+    "url": "./static/css/2.e515a48f.chunk.css"
+  },
+  {
+    "revision": "ae8007a52150b45dcbe4",
+    "url": "./static/css/main.2b3b4588.chunk.css"
+  },
+  {
+    "revision": "a1a693d4f01e1e214d46",
+    "url": "./static/js/2.c591c061.chunk.js"
+  },
+  {
+    "revision": "ae8007a52150b45dcbe4",
+    "url": "./static/js/main.d922aa51.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "cf2842ec05dc062e6a7c67ff09e83fd8",
+    "url": "./static/media/Acme-Regular.cf2842ec.ttf"
+  },
+  {
+    "revision": "82be7c713e89b94d2a6f6a8c16bacf03",
+    "url": "./static/media/Acme-corp.82be7c71.png"
+  }
+]);
