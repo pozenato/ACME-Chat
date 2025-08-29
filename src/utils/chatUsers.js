0631794745f98d8b0b03d4db4a949dcd6be9ffa3
Bruno Pozenato <bruno@busify.com>
2025-08-29 10:57:27 -0300

    Changed the URL to caracteres

--- src/utils/chatUsers.js
+++ src/utils/chatUsers.js
@@ -2,51 +2,51 @@ export const usersMacked = [
     {   
         id: 1, 
         name: "Bugs Bunny", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/1/17/Bugs_Bunny.svg/revision/latest/scale-to-width-down/300?cb=20210521115524"     
+        image: "https://img.favpng.com/7/8/18/bugs-bunny-easter-bunny-vector-graphics-stock-photography-clip-art-png-favpng-QH2f17bvXahtv3naX4J5icSf5.jpg"     
     },
     {   
         id: 2, 
         name: "Tweety", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/9/9c/Tweety_Looney_Tunes_1.png/revision/latest/scale-to-width-down/300?cb=20210521115640"     
+        image: "https://img.favpng.com/3/8/17/tweety-sylvester-high-definition-video-wallpaper-png-favpng-RfTzfg6eJjsZUWLPzEEYueTMC.jpg"     
     },
     {   
         id: 3, 
         name: "Daffy Duck", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/5/5b/Daffy_Duck.svg/revision/latest/scale-to-width-down/300?cb=20210521115524"     
+        image: "https://img.favpng.com/22/10/8/daffy-duck-donald-duck-porky-pig-bugs-bunny-looney-tunes-png-favpng-ZCHdiiXXLbUpdJVA7aLG7t8n5.jpg"     
     },
     {   
         id: 4, 
         name: "Tasmanian Devil", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/1/15/Taz_Looney_Tunes.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
+        image: "https://www.vhv.rs/dpng/d/412-4124864_tasmanian-devil-png-image-file-demonio-de-tasmania.png"     
     },
     {   
         id: 5, 
         name: "Porky Pig", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/5/5a/Porky_Pig.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
+        image: "https://i.pinimg.com/736x/8f/06/f9/8f06f962d2ca4f975ddcf6aeafe46f21.jpg"     
     },
     {   
         id: 6, 
         name: "Marvin the Martian", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/4/4b/Marvin_the_Martian.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
+        image: "https://www.clipartmax.com/png/small/38-381003_marvin-the-martian-images-marvin-the-martian-vector.png"     
     },
     {   
         id: 7, 
         name: "Elmer Fudd", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/7/7b/Elmer_Fudd.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
+        image: "https://www.clipartmax.com/png/small/137-1370690_elmer-fudd-wikipedia-hunting-we-will-go.png"     
     },
     {   
         id: 8, 
         name: "Yosemite Sam", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/6/6c/Yosemite_Sam.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
+        image: "https://www.clipartmax.com/png/small/201-2013868_yosemite-sam-vector-yosemite-sam.png"     
     },
     {   
         id: 9, 
         name: "Road Runner", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/7/7b/Road_Runner.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
+        image: "https://www.clipartmax.com/png/small/481-4818641_roadrunner-clipart-love-road-runner-looney-tunes-running.png"     
     },
     {   
         id: 10, 
         name: "Wile E. Coyote", 
-        image: "https://static.wikia.nocookie.net/looneytunes/images/6/60/Wile_E._Coyote.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
+        image: "https://www.clipartmax.com/png/small/348-3483268_wile-e-coyote-transparent-background.png"     
     }
 ];
