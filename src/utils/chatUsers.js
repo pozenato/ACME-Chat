1b6083cafe5f3538d69f136a31547fdef5326250
Bruno Pozenato <bruno@busify.com>
2025-08-29 10:21:06 -0300

    Changed the caracteres images

--- src/utils/chatUsers.js
+++ src/utils/chatUsers.js
@@ -2,51 +2,51 @@ export const usersMacked = [
     {   
         id: 1, 
         name: "Bugs Bunny", 
-        image: "/images/bugs_bunny.jpg"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/1/17/Bugs_Bunny.svg/revision/latest/scale-to-width-down/300?cb=20210521115524"     
     },
     {   
         id: 2, 
         name: "Tweety", 
-        image: "/images/tweety.png"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/9/9c/Tweety_Looney_Tunes_1.png/revision/latest/scale-to-width-down/300?cb=20210521115640"     
     },
     {   
         id: 3, 
         name: "Daffy Duck", 
-        image: "/images/daffy_duck.png"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/5/5b/Daffy_Duck.svg/revision/latest/scale-to-width-down/300?cb=20210521115524"     
     },
     {   
         id: 4, 
         name: "Tasmanian Devil", 
-        image: "/images/tasmanian_devil.png"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/1/15/Taz_Looney_Tunes.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
     },
     {   
         id: 5, 
         name: "Porky Pig", 
-        image: "/images/porky_pig.png"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/5/5a/Porky_Pig.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
     },
     {   
         id: 6, 
         name: "Marvin the Martian", 
-        image: "/images/marvin.png"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/4/4b/Marvin_the_Martian.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
     },
     {   
         id: 7, 
         name: "Elmer Fudd", 
-        image: "/images/elmer_fudd.png"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/7/7b/Elmer_Fudd.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
     },
     {   
         id: 8, 
         name: "Yosemite Sam", 
-        image: "/images/yosemite_sam.png"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/6/6c/Yosemite_Sam.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
     },
     {   
         id: 9, 
         name: "Road Runner", 
-        image: "/images/road_runner.png"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/7/7b/Road_Runner.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
     },
     {   
         id: 10, 
         name: "Wile E. Coyote", 
-        image: "/images/wile_e_coyote.png"     
+        image: "https://static.wikia.nocookie.net/looneytunes/images/6/60/Wile_E._Coyote.svg/revision/latest/scale-to-width-down/300?cb=20210521115640"     
     }
 ];
